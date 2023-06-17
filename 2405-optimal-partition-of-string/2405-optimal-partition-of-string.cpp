54ca7fd619fd6aea25a612928d20a5e1e35bedb4
Subhadeep Das <76871085+subhadeep01@users.noreply.github.com>
2023-06-18 00:51:13 +0530

    Time: 236 ms (49.91%), Space: 44.5 MB (56.18%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int partitionString(string s) {
-        map<char,int>m;
+        unordered_map<char,int>m;
         int ans=0;
         for(int i=0;i<s.size();i++)
         {
