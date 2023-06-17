1b12a0d4834fc742d6abb61a5b73aff73a09efb9
Subhadeep Das <76871085+subhadeep01@users.noreply.github.com>
2023-06-18 01:44:47 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (62.10%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -2,17 +2,6 @@ class Solution {
 public:
     int hIndex(vector<int>& citations) {
         int n=citations.size();
-
-        // int f=0;
-        // for(auto x: citations)
-        // {
-        //     if(x!=0)
-        //     {
-        //         f=1;
-        //         break;
-        //     }
-        // }
-        // if(f==0) return 0;
         sort(citations.begin(),citations.end());
         int low=0,high=citations.back();
         int ans=0;
