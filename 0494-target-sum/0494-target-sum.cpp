5d2d2a9cb6f2726e6d5719661cecf6d1e4f44537
Subhadeep Das <76871085+subhadeep01@users.noreply.github.com>
2023-06-21 21:43:39 +0530

    Time: 18 ms (64.10%), Space: 12.1 MB (42.54%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+     int f(int ind, int target, vector<int>&a, vector<vector<int>>&dp)
+{
+    
+       if(ind == 0){
+        if(target==0 && a[0]==0)
+            return 2;
+        if(target==0 || target == a[0])
+            return 1;
+        return 0;
+    }
+   if(dp[ind][target]!=-1)return dp[ind][target];
+   int take=0;
+   if(a[ind]<=target)
+    take= f(ind-1, target-a[ind],a, dp);
+   int not_take=f(ind-1, target,a,dp);
+   return dp[ind][target]= (take + not_take);
+}
+int countPartitions(int n, int d, vector<int> &arr) {
+    // Write your code here.
+    // s1+s2=sum
+    //s1-s2=d
+    int sum=0;
+    for(auto x: arr) sum+=x;
+    if((sum-d)<0) return 0; 
+    if((sum-d)%2==1)return 0;
+    sum=(sum-d)/2;
+    vector<vector<int>>dp(n+1,vector<int>(sum+1,-1));
+    return f(n-1,sum,arr,dp);
+}
+   
+    int findTargetSumWays(vector<int>&nums ,int target) {
+        //Your code here
+        int n=nums.size();
+        return countPartitions(n,target,nums);
+    }
+};
