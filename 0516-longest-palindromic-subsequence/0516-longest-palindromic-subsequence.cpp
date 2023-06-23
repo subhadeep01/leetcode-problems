f117354e95b5d6d4af58c4c0a7833b43f4a27847
Subhadeep Das <76871085+subhadeep01@users.noreply.github.com>
2023-06-23 15:27:46 +0530

    Time: 348 ms (6.02%), Space: 82.1 MB (11.38%) - LeetHub

---
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int longestPalindromeSubseq(string s1) {
+        string s2;
+        for(auto x: s1)
+        s2=x+s2;
+        int n=s1.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        for(int i=0;i<=n;i++)
+        {
+            for(int j=0;j<=n;j++)
+            {
+                if(i==0 || j==0) continue;
+                if(s1[i-1]==s2[j-1])
+                {
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][n];
+    }
+};
