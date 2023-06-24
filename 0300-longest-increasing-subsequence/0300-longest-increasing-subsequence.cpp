bf1e1cad3800c54e4569c91334080e51d203cfe7
Subhadeep Das <76871085+subhadeep01@users.noreply.github.com>
2023-06-24 14:27:57 +0530

    Time: 1673 ms (8.54%), Space: 292.4 MB (7.62%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int LCS(int ind, int prev_ind, vector<int>&nums, vector<vector<int>>&dp)
+    {
+        if(ind==nums.size())return 0;
+        if(dp[ind][prev_ind+1]!=-1) return dp[ind][prev_ind+1];
+        int not_take=0+LCS(ind+1,prev_ind,nums,dp);
+        int take=0;
+        if(prev_ind==-1 || nums[ind]>nums[prev_ind])
+        take=1+LCS(ind+1,ind,nums,dp);
+        return dp[ind][prev_ind+1]=max(take,not_take); 
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        vector<vector<int>>dp(nums.size()+1,vector<int>(nums.size()+1,-1));
+        return LCS(0,-1,nums,dp);
+    }
+};
