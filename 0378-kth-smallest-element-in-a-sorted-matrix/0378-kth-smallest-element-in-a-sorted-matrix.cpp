4730c3f592a35cf02dcffbe7e20a2bf5615e17c2
Subhadeep Das <76871085+subhadeep01@users.noreply.github.com>
2023-06-29 20:49:36 +0530

    Time: 25 ms (96.16%), Space: 12.9 MB (99.18%) - LeetHub

---
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int count(vector<vector<int>>& matrix, int x)
+    {
+        // this function will return how many elements are <= x 
+        int cnt=0;
+        for(int i=0;i<matrix.size();i++)
+        {
+            cnt+=upper_bound(matrix[i].begin(),matrix[i].end(),x)-matrix[i].begin();
+        }
+        return cnt;
+    }
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int n=matrix.size();
+        int low=matrix[0][0];
+        int high=matrix[n-1][n-1];
+        while(low<high)
+        {
+            int mid=(low+high)>>1;
+            
+            if(count(matrix,mid)<k)
+                low=mid+1;
+            else
+                high=mid;
+        }
+        return low;
+    }
+};
