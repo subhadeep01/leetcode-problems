9d090850f18b74b9ca30d36292f252f2b7748f96
Subhadeep Das <76871085+subhadeep01@users.noreply.github.com>
2023-07-02 17:08:03 +0530

    Time: 2149 ms (5.88%), Space: 412.2 MB (5.88%) - LeetHub

---
+++ 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int ans=0;
+    void f(int i, vector<vector<int>>& requests,vector<int>person,int request_accepted)
+    {
+          if(i==requests.size())
+          {
+              for(auto x: person)
+              {
+                  if(x!=0)return;
+              }
+              ans=max(ans,request_accepted);
+              return;
+          }
+        
+        
+         
+          //if we take current request
+          person[requests[i][0]]--;
+          person[requests[i][1]]++;
+          f(i+1,requests,person,request_accepted+1);
+        
+          //backtrack;
+          person[requests[i][0]]++;
+          person[requests[i][1]]--;
+        
+        
+          // if we dont take the current request
+          f(i+1,requests,person,request_accepted);
+    }
+    int maximumRequests(int n, vector<vector<int>>& requests) {
+        vector<int>person(n,0);
+        // assume person in every building to be full that is 0
+        f(0,requests,person,0);
+        return ans;
+    }
+};
