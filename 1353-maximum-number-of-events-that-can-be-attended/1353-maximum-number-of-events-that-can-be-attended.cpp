f0bd0484c97536a36544b79d8bbfcc7f01bd2b22
Subhadeep Das <76871085+subhadeep01@users.noreply.github.com>
2023-07-15 16:10:21 +0530

    Time: 2633 ms (5.01%), Space: 428.2 MB (5.15%) - LeetHub

---
+++ 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    static bool cmp(vector<int>&a, vector<int>&b)
+    {
+        if(a[1]<b[1]) return true;
+        if(a[1]>b[1])return false;
+        if(a[0]<b[0]) return true;
+        return false;
+    }
+    int maxEvents(vector<vector<int>>& events) {
+        int no_of_events=events.size();
+        sort(events.begin(),events.end(),cmp); // sort them according to their end
+        // if end is equal sort them according to their start time
+        int ans=0;
+        set<int>days;
+        for(int i=1;i<=100000;i++)
+        days.insert(i);
+        for(auto event: events)
+        {
+            int start=event[0];
+            int end=event[1];
+            auto nearest_day=days.lower_bound(start);
+            if(nearest_day==days.end() || *nearest_day> end) // if day not in the set or nearest day is greater than end date then cant attend that event
+                continue;
+            else
+            {
+                ans++;
+                days.erase(nearest_day);// remove that day from set since its already occupied
+            }
+        }
+        
+        return ans;
+    }
+};
