665abcbb990e43c4ba2747eff94c550ff83d208e
Subhadeep Das <76871085+subhadeep01@users.noreply.github.com>
2023-07-21 20:19:07 +0530

    Time: 385 ms (88.85%), Space: 77.5 MB (47.63%) - LeetHub

---
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxEnvelopes(vector<vector<int>>&envelopes) {
+        int n=envelopes.size();
+         sort(envelopes.begin(), envelopes.end(), [](const auto& a, const auto& b) {
+            return a[0] < b[0] || (a[0] == b[0] && a[1] > b[1]);
+        });
+
+        // we have to do it in O(nlogn) by binary search
+        vector<int>temp;
+        temp.push_back(envelopes[0][1]);
+        for(int i=1;i<n;i++)
+        {
+            if(envelopes[i][1]>temp.back())
+            {
+                temp.push_back(envelopes[i][1]);
+            }
+            else
+            {
+                int ind = lower_bound(temp.begin(),temp.end(),envelopes[i][1])-temp.begin();
+                temp[ind]=envelopes[i][1];
+            }
+        }
+        return temp.size();
+    }
+};
